d753a7c6130a96bebf9d086e7fe53db8f917283b
binaya.parajuli <binaya.parajuli@nicnepal.org>
2024-09-29 08:36:05 +0545

    feat(auth in tenant app)

--- shared/admin.py
+++ shared/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Client, Domain
+from django_tenants.admin import TenantAdminMixin
 
-admin.site.register(Client)
-admin.site.register(Domain)
+@admin.register(Client)
+class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ("name",)
+    
+    
+@admin.register(Domain)
+class DomainAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ("domain","tenant")
